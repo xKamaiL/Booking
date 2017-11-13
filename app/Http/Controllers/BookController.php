1642d576567d6fb619d3812b05d7a2b544aed371
xKamaiL <camel4882@gmail.com>
2017-11-13 19:37:38 +0700

    Bookcontroller @search

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -15,5 +16,28 @@ class BookController extends Controller
         #book_category
         $cat = Category::query()->get();
         return view("frontend.book.index",["cat" => $cat]);
+    }
+    public function search(Request $request){
+        $input = $request->all();
+        if($input["search"] == null){
+            switch ($input["cat"]){
+                case "all":
+                    $data = Book::query()->get();
+                    break;
+                default:
+                    $data = Book::query()->where("cat_id","=",$input["cat"])->get();
+                    break;
+            }
+        }else{
+            switch ($input["cat"]){
+                case "all":
+                    $data = Book::query()->where("name","LIKE","%".$input["search"]."%")->get();
+                    break;
+                default:
+                    $data = Book::query()->where("name","LIKE","%".$input["search"]."%")->where("cat_id","=",$input["cat"])->get();
+                    break;
+            }
+        }
+        return view("frontend.book.result",["data" => $data]);
     }
 }
