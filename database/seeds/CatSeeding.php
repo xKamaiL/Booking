b50d415851e2e0dfb64bbf8d67ef6041d35a64a5
xKamaiL <camel4882@gmail.com>
2017-11-13 19:38:33 +0700

    cat seeding

--- database/seeds/CatSeeding.php
+++ database/seeds/CatSeeding.php
@@ -11,6 +11,23 @@ class CatSeeding extends Seeder
      */
     public function run()
     {
-        //
+        \App\Models\Category::query()->insert([
+            "name" => "เบ็ดเตล็ด"
+        ]);
+        \App\Models\Category::query()->insert([
+            "name" => "ปรัชญา"
+        ]);
+        \App\Models\Category::query()->insert([
+            "name" => "สังคมศาสตร์"
+        ]); \App\Models\Category::query()->insert([
+        "name" => "วิทยาศาสตร์"
+    ]);
+        \App\Models\Category::query()->insert([
+            "name" => "ศิลปกรรมเเละการบันเทิง"
+        ]); \App\Models\Category::query()->insert([
+        "name" => "วรรณคดี"
+    ]); \App\Models\Category::query()->insert([
+        "name" => "ประวัติศาสตร์เเละภูมิศาสตร์"
+    ]);
     }
 }
